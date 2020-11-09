6673ba7add51fdc7ce32dbc8ebaa907873963ccb
nxtfsm <sysadmin@xaviermicronesia.org>
2020-11-09 12:58:41 -0600

    Parse props to AccordionItems

--- src/App/components/ContentWindow/_AccordionViewer/_AccordionViewer.js
+++ src/App/components/ContentWindow/_AccordionViewer/_AccordionViewer.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Accordion, AccordionItem } from 'carbon-components-react';
+import React, { useState } from 'react';
+import { Accordion, AccordionItem, Link } from 'carbon-components-react';
 
 import placeholderData from './_placeholderData';
 
 export default function AccordionViewer({ props }) {
-  const { content } = props;
+  const { content } = props,
+        [accordionItems] = useState( loadItems(placeholderData) );
 
   return (
-    <div>
-      { content && content.defaultStr }
-    </div>
+    <Accordion children={ accordionItems } />
   )
+
+  function loadItems(fromList) {
+    return fromList.map((item, idx) => {
+      const key = `accordionItem_${item.id}`,
+            props = {
+              key,
+              headline: item.itemHeadline,
+              body: item.belowTheFoldLabel,
+              url: item.externalUrl
+            }
+        return (
+          <AccordionItem
+            key={ key }
+            title={ <h3>{item.itemHeadline}</h3> } >
+            <p>{ item.belowTheFoldLabel }</p>
+            <Link
+              size='lg'
+              target="_blank"
+              href={ item.externalUrl }
+              children={ item.externalUrlDisplay } />
+
+          </AccordionItem>
+        )
+    })
+  }
 }
