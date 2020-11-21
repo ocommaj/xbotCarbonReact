42796d446d9e6eeeea0500b57841b62ef903d315
nxtfsm <sysadmin@xaviermicronesia.org>
2020-11-21 14:19:24 -0600

    Cleaner toggle constructor in footerControls

---
+++ src/models/UserRecord/_reducers.js
@@ -0,0 +1,27 @@
+export default {
+  apolloRequest: (auth0userRecord) => {
+    return {
+      atXavierAccount: auth0userRecord.email,
+      verifiedEmail: auth0userRecord.email_verified,
+      user: {
+        atXavierAccount: auth0userRecord.email,
+        firstName: auth0userRecord.given_name,
+        familyName: auth0userRecord.family_name,
+        fullName: auth0userRecord.name,
+        goesBy: auth0userRecord.nickname || '',
+        lastLogin: new Date()
+      }
+    }
+  },
+  apolloResponse: (apolloUserRecord) => {
+    return {
+      atXavierAccount: apolloUserRecord.atXavierAccount,
+      firstName: apolloUserRecord.firstName,
+      familyName: apolloUserRecord.familyName,
+      fullName: `${apolloUserRecord.firstName} ${apolloUserRecord.familyName}`,
+      goesBy: apolloUserRecord.goesBy || '',
+      isEditor: apolloUserRecord.isEditor,
+      lastLogin: new Date().toUTCString(apolloUserRecord.lastLogin)
+    }
+  }
+}
