2c0d6723313297a56e660e45fde2bbc46f095070
nxtfsm <sysadmin@xaviermicronesia.org>
2020-11-22 21:22:35 -0600

    refactor mainContentRow to clean useCallback->useEffect chain

--- src/App/containers/_MainBodyGrid/_MainContentRow/_MainContentRow.js
+++ src/App/containers/_MainBodyGrid/_MainContentRow/_MainContentRow.js
@@ -2,32 +2,39 @@ import React, { useState, useCallback, useEffect } from 'react';
 import ContentWindow, { templates } from '@components/ContentWindow';
 
 export default function MainContentRow(props) {
-  const { sections, activeSectionId, animate } = props,
+  const { animate, sections, activeSectionId } = props,
+        [ nextWindow, setNextWindow ] = useState(null),
         [ contentWindow, setContentWindow ] = useState(null);
+
+  const activeSection = sections[activeSectionId];
+
+  const loadTemplate = useCallback(() => {
+      if (!activeSection) return
+
+      const template = templates[activeSection.windowType];
+      setNextWindow( template({
+              'animate': animate.content,
+              'mainId': activeSection.mainId,
+              'title': activeSection.defaultStr,
+              'tabs': activeSection.options.tabs,
+            })
+          );
+  }, [animate, setNextWindow, activeSection])
+
+  const loadContentWindow = useCallback(() => {
+    setContentWindow(prevState => !prevState
+       ? nextWindow
+       : animate.collapse( () => setContentWindow(nextWindow) )
+    )
+  }, [animate, nextWindow, setContentWindow])
 
   const displayContentWindow = useCallback(() => {
     if (contentWindow) { animate.reveal() }
   }, [animate, contentWindow])
 
-  useEffect(() => loadContentWindow(), [activeSectionId] )
+  useEffect(() => loadTemplate(), [loadTemplate])
+  useEffect(() => loadContentWindow(), [loadContentWindow])
   useEffect(() => displayContentWindow(), [displayContentWindow])
-
-  function loadContentWindow() {
-    if (!activeSectionId) return
-    const activeSection = sections[activeSectionId];
-
-    const props = {
-            'animate': animate.content,
-            'mainId': activeSection.mainId,
-            'title': activeSection.defaultStr,
-            'tabs': activeSection.options.tabs,
-          },
-          template = templates[activeSection.windowType],
-          toLoad = template(props);
-
-    if (contentWindow) { animate.collapse(() => { setContentWindow(toLoad) }) }
-        else { setContentWindow(toLoad) }
-    }
 
   if (!activeSectionId) return null
 
