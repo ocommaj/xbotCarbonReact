603d6ec02e0b16eeb48f7a09307ad693e1c3caf2
nxtfsm <sysadmin@xaviermicronesia.org>
2021-01-06 14:31:06 -0600

    connects to heroku hosted mongodb

--- src/Providers/_ApolloProvider.js
+++ src/Providers/_ApolloProvider.js
@@ -3,7 +3,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 export default function Apollo({ children }) {
   const apolloClient = new ApolloClient({
-    uri: "http://localhost:4002/graphql",
+    //uri: "http://localhost:4002/graphql",
+    uri: 'https://fathomless-waters-14572.herokuapp.com/graphql',
     cache: new InMemoryCache()
   });
 
